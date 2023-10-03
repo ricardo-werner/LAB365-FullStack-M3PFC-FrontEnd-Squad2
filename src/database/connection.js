d63c50933cf8de5bc9419ba386faad702d7074ea
ricardo-werner <wernerdive@gmail.com>
2023-10-03 01:31:26 -0300

    delete all files in git

--- src/database/connection.js
+++
@@ -1,6 +0,0 @@
-const { Sequelize } = require('sequelize');//Importação do Sequelize para utilização de seus métodos
-const databaseConfig = require('../config/database.config');//Importação das configurações do banco de dados
-
-const connection = new Sequelize(databaseConfig);//Criação da conexão com o banco de dados
-
-module.exports = { connection }; //Exportação da conexão para utilização em outros arquivos
--- src/index.js
+++
@@ -1,3 +0,0 @@
-const { Server } =require('./server');
-
-new Server();//Inicialização da classe Server para executar o servidor
