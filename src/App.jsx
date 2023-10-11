d5e7e6493005f538fadd71e77d64a2afb6aa8487
ricardo-werner <wernerdive@gmail.com>
2023-10-11 00:17:06 -0300

    feat: testes de @mui/material-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+//import React, { useState } from 'react';
 import './App.css';
 import AppRouter from './Router';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
 
 function App() {
+  //const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
+      <Box m={10}>
+        <Button />
+      </Box>
 
       <AppRouter />
 
