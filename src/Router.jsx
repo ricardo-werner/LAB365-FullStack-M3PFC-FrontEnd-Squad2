cf9737e1732c5dec07bcd87db9f5141a4f0cc05c
ricardo-werner <wernerdive@gmail.com>
2023-10-12 20:38:02 -0300

    feat: criado rotas dos Medicamentos e Finalizar

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-
+import FinalizarCompraPage from "./pages/FinalizarCompra/FinalizarCompraPage";
+import Medicamentos from './pages/Medicamentos/Medicamentos';
+import MedicamentoCreate from './pages/Medicamentos/MedicamentoCreate';
 
 const AppRouter = () => {
 
@@ -7,6 +9,9 @@ const AppRouter = () => {
         <Router>
             <Routes>
                 <Route path="/" element={<Navigate to="#" />} />
+                <Route path="/medicamentos" element={<Medicamentos />} />
+                <Route path="/medicamentos/create" element={<MedicamentoCreate />} />
+                <Route path="/finalizar" element={<FinalizarCompraPage />} />
             </Routes>
         </Router>
     );
