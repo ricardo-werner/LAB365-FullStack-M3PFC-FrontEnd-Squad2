083b71c6e8c2c6dfa90e384daab9effb47ea79e8
ricardo-werner <wernerdive@gmail.com>
2023-10-12 12:12:09 -0300

    refactor: deletado botão logout

--- src/pages/FinalizarCompra/FinalizarCompraPage.jsx
+++ src/pages/FinalizarCompra/FinalizarCompraPage.jsx
@@ -1,27 +1,11 @@
-import { useContext } from 'react';
-import { AuthContext } from '../../contexts/auth';
 import FinalizarCompraCard from '../../components/FinalizarCompra/FinalizarCompraCard';
 import './FinalizarCompra.css';
 
 const FinalizarCompraPage = () => {
-    const { logout } = useContext(AuthContext);
-
-    const handleLogout = () => {
-        logout();
-    }
-
 
     return (
         <div >
-            <h1 className='row mx-4'>
-                Finalizar Compra
-            </h1>
             <FinalizarCompraCard />
-            <button
-                className='btn btn-danger mx-3 my-2'
-                onClick={handleLogout}>
-                Logout
-            </button>
         </div>
     )
 }
