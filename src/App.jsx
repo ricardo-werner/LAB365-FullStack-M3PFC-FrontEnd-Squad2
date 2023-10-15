3f52143e0bbd3850b6420a49710cb2b68fbc5f8c
ricardo-werner <wernerdive@gmail.com>
2023-10-15 13:17:40 -0300

    feat: adicionado Negacao(Header)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css';
-import Navegacao from './components/Navegacao/Navegacao';
+//import Navegacao from './components/Navegacao/Navegacao';
 import AppRouter from './Router';
 
 function App() {
   return (
     <div className="App">
-      <Navegacao />
+      {/*<Navegacao />*/}
       <AppRouter />
     </div>
   );
