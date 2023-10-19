26b0fd89d40785b23b1b0e98e6082d5eebc39a55
Rafael Zampieron <rafaelprimieri@hotmail.com>
2023-10-18 21:46:12 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import mdx from '@mdx-js/rollup'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    define: {
-      "process.env": env,
-    },
-    plugins: [react({ jsxImportSource: "@emotion/react" })],
-  };
-});
+export default defineConfig({
+  plugins: [
+    { enforce: 'pre', ...mdx() },
+    react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
+  ],
+})
