2087957dd445d7b6fbf33f2f39656215dc761b5b
Rafael Zampieron <rafaelprimieri@hotmail.com>
2023-10-18 21:26:35 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({  }) => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
   return {
-    optimizeDeps: {
-      include: ["react/jsx-runtime"],
+    define: {
+      "process.env": env,
     },
     plugins: [react()],
   };
 });
-
