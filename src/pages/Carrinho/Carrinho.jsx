144550f8b532477b2218411a895f770f785e285b
ricardo-werner <wernerdive@gmail.com>
2023-10-23 08:16:04 -0300

    refactor: fixado casas decimal e add rolagem vert.

--- src/pages/Carrinho/Carrinho.jsx
+++ src/pages/Carrinho/Carrinho.jsx
@@ -5,8 +5,13 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function Cart({ mostraModal, toggle }) {
-  const { itensCarrinho, adicionarAoCarrinho, removerDoCarrinho, limparCarrinho, valorTotalCarrinho } =
-    useContext(CartContext);
+  const {
+    itensCarrinho,
+    adicionarAoCarrinho,
+    removerDoCarrinho,
+    limparCarrinho,
+    valorTotalCarrinho
+  } = useContext(CartContext);
 
   const notifyRemovedFromCart = (item) =>
     toast.error(`${item.nomeProduto} removido do carrinho!`, {
@@ -56,7 +61,7 @@ export default function Cart({ mostraModal, toggle }) {
             Fechar
           </button>
         </div>
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4" style={{ overflowY: "auto", maxHeight: "60vh" }}>
           {itensCarrinho.map((item) => (
             <div className="flex justify-between items-center" key={item.id}>
               <div className="flex gap-4">
@@ -101,7 +106,7 @@ export default function Cart({ mostraModal, toggle }) {
         </div>
         {itensCarrinho.length > 0 ? (
           <div className="flex flex-col justify-between items-center">
-            <h1 className="text-lg font-bold">Total: R$ {valorTotalCarrinho()}</h1>
+            <h1 className="text-lg font-bold">Total: R$ {valorTotalCarrinho().toFixed(2)}</h1>
             <button
               className="px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
               onClick={() => {
