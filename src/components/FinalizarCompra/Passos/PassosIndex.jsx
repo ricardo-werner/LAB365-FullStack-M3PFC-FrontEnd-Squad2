2b11c9e936881086c18270d8d0ae7f710e937a27
ricardo-werner <wernerdive@gmail.com>
2023-10-24 21:24:31 -0300

    feat: add FinalizarCompra

--- src/components/FinalizarCompra/Passos/PassosIndex.jsx
+++ src/components/FinalizarCompra/Passos/PassosIndex.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
@@ -8,9 +9,12 @@ import Alert from '@mui/material/Alert';
 import { Produtos } from '../Produtos/ProdutosIndex';
 import { Endereco } from '../Entrega/EntregaIndex';
 import { Pagamento } from '../Pagamentos/PagamentoIndex';
-
+import { CartContext } from '../../../contexts/carrinhoCompras';
 
 export function Passos() {
+  const [compraFinalizada, setCompraFinalizada] = useState(false);
+  const { limparCarrinho } = useContext(CartContext);
+  const navigate = useNavigate();
   const [currentStep, setCurrentStep] = useState(0);
   const [compraEfetuada, setCompraEfetuada] = useState(false);
 
@@ -20,6 +24,12 @@ export function Passos() {
 
   function handleBack() {
     setCurrentStep(currentStep - 1);
+  }
+
+  const FinalizarCompra = () => {
+    localStorage.removeItem('itensCarrinho');
+    setCompraFinalizada(true);
+    limparCarrinho();
   }
 
   return (
@@ -40,29 +50,49 @@ export function Passos() {
       {currentStep === 1 && !compraEfetuada && <Endereco />}
       {currentStep === 2 && !compraEfetuada && <Pagamento />}
 
-      {currentStep > 0 && !compraEfetuada && (
-        <Button onClick={handleBack}>
-          Voltar
-        </Button>
-      )}
+      <div className="flex justify-center gap-12">
+        {currentStep > 0 && !compraEfetuada && (
+          <Button
+            className="m-3 px-4 py-2 text-black text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700" style={{ backgroundColor: 'rgb(32,193,148)' }}
+            onClick={handleBack}>
+            Voltar
+          </Button>
+        )}
 
-      {currentStep < 2 && !compraEfetuada && (
-        <Button onClick={handleNext}>
-          Próximo
-        </Button>
-      )}
+        {currentStep < 2 && !compraEfetuada && (
+          <Button
+            className="m-3 px-4 py-2 text-black text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700" style={{ backgroundColor: 'rgb(32,193,148)' }}
+            onClick={handleNext}>
+            Próximo
+          </Button>
+        )}
 
-      {currentStep === 2 && !compraEfetuada && (
-        <Button onClick={() => setCompraEfetuada(true)}>
-          Finalizar
-        </Button>
-      )}
+        {currentStep === 2 && !compraEfetuada && (
+          <Button
+            className="m-3 px-4 py-2 text-black text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700" style={{ backgroundColor: 'rgb(32,193,148)' }}
+            onClick={() => {
+              setCompraEfetuada(true);
+              FinalizarCompra();
+            }}>
+            Finalizar
+          </Button>
+        )}
 
-      {compraEfetuada && (
-        <Alert severity="success">
-          Compra efetuada com sucesso! Confira seu e-mail para mais informações.
-        </Alert>
-      )}
+        {compraEfetuada && (
+          <>
+            <Alert severity="success">
+              Compra efetuada com sucesso! Confira seu e-mail para mais informações.
+            </Alert>
+            <Button
+              className="mt-6 px-4 py-2 text-black text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700" style={{ backgroundColor: 'rgb(32,193,148)' }}
+              onClick={() => {
+                navigate("/comprador/medicamentos");
+              }}>
+              Nova Compra
+            </Button>
+          </>
+        )}
+      </div>
     </Box >
   );
 }
